f0d7dd4b580d9a0a24b6a02c9acb9c0dc6d306fe
gowriganesh-voonna <gowriganesh.voonna@thezennialproc.com>
2025-07-02 20:06:24 +0530

    Url Parsing Code

---
+++ gpt4_news_processor/worker/url_parse.py
@@ -0,0 +1,30 @@
+from urllib.parse import urlparse,parse_qs ,urlencode,urlunparse
+
+#url to parse
+
+url = "https://www.geeksforgeeks.org/python/python-pillow-a-fork-of-pil/"
+
+parsed = urlparse(url)
+
+print(parsed)
+
+#output :
+#ParseResult(scheme='https', netloc='www.geeksforgeeks.org',
+#  path='/python/python-pillow-a-fork-of-pil/', params='', query='', fragment='')
+
+query_dict = parse_qs(parsed.query)
+query_dict['q']=['python tutorial']
+query_dict['page'] =['2']
+
+new_query = urlencode(query_dict,doseq=True)
+#rebuilding new url
+new_url = urlunparse((
+    parsed.scheme,
+    parsed.netloc,
+    parsed.path,
+    parsed.params,
+    new_query,
+    parsed.fragment   
+))
+
+print("New Url :",new_url)
