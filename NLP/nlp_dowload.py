26c656e11333d8c644e45113d42dd5e27f766f0c
gowriganesh-voonna <gowriganesh.voonna@thezennialproc.com>
2025-07-14 18:08:13 +0530

    Basics of NLP

--- NLP/nlp_dowload.py
+++ NLP/nlp_dowload.py
@@ -1,5 +1,34 @@
 from nltk.book import *
 
+
+
 # concordance view shows us every occurrence of a given word, together with some
 #context.
 text1.concordance("monstrous")   
+
+text2.concordance("book")
+
+print("--------------------------")
+
+text1.similar("monstrous")
+
+text2.similar("book")
+
+
+print("-----------------------------")
+
+text3.common_contexts(["monstrous","very"])
+
+
+#text4.dispersion_plot(["citizens", "democracy", "freedom", "duties", "America"])
+
+#text3.generate()
+
+print(len(text3))
+
+
+# sorted 
+print("------------------sorted--------------------")
+print(sorted(set(text3)))
+
+print(f"length : {len(sorted(set(text3)))}")
