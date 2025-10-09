fa71cd3fec345eeaf977a98ea7cbc637b1fee2e6
gowriganesh-voonna <gowriganesh.voonna@thezennialproc.com>
2025-10-09 12:59:59 +0530

    Document loaders code done

---
+++ google_gemini/Document_loaders/PPT_loader.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage,SystemMessage
+import os
+
+#specify the path
+loader = UnstructuredPowerPointLoader("Role_based_task_tracking.pptx")
+
+
+
+#load document
+documents = loader.load()
+
+llm= ChatGoogleGenerativeAI(
+    model = "gemini-2.5-pro",
+    api_key = os.getenv("GEMINI_API_KEY")
+
+)
+
+
+text = [doc.page_content for i,doc in enumerate(documents)]
+
+query = input("Data fetching done. Please enter your Query : ").strip()
+messages = [
+    SystemMessage(content="You are a helpful assistant.Please answer the query from the given text"),
+    HumanMessage(content=f"{text}  \n Query : {query}")
+]
+response = llm.invoke(messages)
+print(response.content)
+# content
+
+# for i,doc in enumerate(documents):
+#     print(f"\n Document {i+1}")
+#     print("Content: ",doc.page_content)
+#     print("Metadata: ",doc.metadata)
